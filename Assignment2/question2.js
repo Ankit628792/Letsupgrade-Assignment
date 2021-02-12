6d0e2c90c15668b3317612892c16fa95e72f2f34
Ankit <ankit628792@gmail.com>
2021-02-12 20:32:01 +0530

    Assignment2 of LetsUgrade

---
+++ Assignment2/question2.js
@@ -0,0 +1,16 @@
+
+/*
+Write a JavaScript program to display a multiplication table of 5 and print it in the console in the
+given format:
+5*1=5
+5*2=10
+5*3=15
+.
+.
+5*20=10
+
+*/
+
+//code for program
+for(let i=1; i<=10; i++)
+            console.log("5"+"*"+i+"="+(5*i));
